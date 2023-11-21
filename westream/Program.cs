d141671c27c60d7ef418e062def93385bd8af4bf
Rodney <rodney002mwanje@gmail.com>
2023-11-22 00:05:20 +0300

    feat: WebSocket server using WebSocketSharp

--- westream/Program.cs
+++ westream/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace csharpserver;
+
+class Program{
+    static void Main(string[] args) {
+        WebSocketServer wssv = new WebSocketServer("ws://127.0.0.1:7890");
+
+        wssv.Start();
+        Console.WriteLine("Started WebSocket Server at 127.0.0.1:7890");
+
+        Console.ReadKey();
+        wssv.Stop();
+
+        Console.WriteLine("Stopped the Server");
+    }
+}
