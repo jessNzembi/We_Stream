fc675548e501047f77fc026ac1f15b931a6ac9ae
Rodney <rodney002mwanje@gmail.com>
2023-11-22 00:35:48 +0300

    feat: communication between server and client

--- westream/Program.cs
+++ westream/Program.cs
@@ -4,12 +4,22 @@ using WebSocketSharp.Server;
 
 namespace csharpserver;
 
+class Echo: WebSocketBehavior {
+    protected override void OnMessage(MessageEventArgs e) {
+        Console.WriteLine("Received Message From Client: " + e.Data);
+        Send(e.Data);
+    }
+}
+
 class Program{
     static void Main(string[] args) {
         WebSocketServer wssv = new WebSocketServer("ws://127.0.0.1:7890");
 
         wssv.Start();
         Console.WriteLine("Started WebSocket Server at 127.0.0.1:7890");
+
+        // adding servives or behaviours as routes
+        wssv.AddWebSocketService<Echo>("/Echo");
 
         Console.ReadKey();
         wssv.Stop();
