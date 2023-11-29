9b5d1d2583ae33487f6557e1426d342cfae65b71
Rodney <rodney002mwanje@gmail.com>
2023-11-30 00:24:06 +0300

    feat: modifications to player class

--- westream/Core/MusicPlayer/MusicPlayer.cs
+++ westream/Core/MusicPlayer/MusicPlayer.cs
@@ -5,31 +5,31 @@ namespace MusicPlayer
 {
     public class MusicPlayer
     {
-        public static Player createPlayer() {
-            var player = new Player();
+        private var player = new Player();
+        public static void setPlayBackFinish() {
             player.PlaybackFinished += OnPlaybackFinished;
-            return player
         }
 
-        public static void PlayMusic(Player musicplayer, string filename) {
-            musicplayer.Play(fileName).Wait();
+        public static void PlayMusic(string filename) {
+            player.Play(fileName).Wait();
         }
 
-        public static void PauseMusic(Player musicplayer) {
-            musicplayer.Pause().Wait();
+        public static void PauseMusic() {
+            player.Pause().Wait();
         }
 
-        public static void ResumeMusic(Player musicPlayer) {
+        public static void ResumeMusic() {
             player.Resume().Wait();
         }
 
-        public static void AdjustVolume(Player musicplayer) {
-            byte volume = Convert.ToByte(Console.ReadLine());
+        public static void AdjustVolume(string percentage) {
+            byte volume = Convert.ToByte(percentage);
             player.SetVolume(volume).Wait();
         }
 
         private static void OnPlaybackFinished(object sender, EventArgs e)
         {
+            // be used to play next songs that are added to the list
             Console.WriteLine("Playback finished");
         }
     }
