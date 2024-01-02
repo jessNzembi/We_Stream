39d68d93d86f54ad22ef158fd102282a7765afd0
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-01-02 17:02:21 +0300

    fix: fixed merge conflicts

--- westream/Main.cs
+++ westream/Main.cs
@@ -1,17 +1,26 @@
-﻿/*
- * File: Main.cs
- * Description: Entry Point of the program
- * Author: The group members
- */
+using System;
+using csharpserver;
+using csharpclient;
 
-using Westream;
+namespace ProgramEntry;
 
-public class Program
-{
-  public static void Main(String[] args)
-  {
-    Console.WriteLine("Hello from we stream :)\n");
-    WestreamGui gui = new WestreamGui();
-    gui.Run();
-  }
+class ProgramEntry{
+    static void Main(string[] args) {
+        Console.WriteLine("Welcome to WeStream");
+        Console.WriteLine("Press 1: To Create Channel\nPress 2: To Join Channel");
+        string? user_input = Console.ReadLine();
+        switch(user_input) {
+            case "1":
+                Console.WriteLine("Creating Server");
+                Program.StartServer();
+                break;
+            case "2":
+                Console.WriteLine("Joining Channel");
+                SocketClient.StartClient();
+                break;
+            default:
+                Console.WriteLine("Opps Wrong Choice");
+                break;
+        }
+    }
 }
