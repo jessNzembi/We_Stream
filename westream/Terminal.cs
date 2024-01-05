69afbe56971e3d4f76f1084b28c5991585261e75
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-01-06 02:41:58 +0300

    re: renamed Main.cs to Terminal.cs

---
+++ westream/Terminal.cs
@@ -0,0 +1,29 @@
+using System;
+using csharpserver;
+using csharpclient;
+
+namespace ProgramEntry;
+
+class ProgramEntry{
+    static void Main(string[] args) {
+        Console.WriteLine("Welcome to WeStream");
+        Console.WriteLine("Press 1: To Create Channel\nPress 2: To Join Channel");
+        string? user_input = Console.ReadLine();
+        switch(user_input) {
+            case "1":
+                Console.WriteLine("Creating Server");
+                Program.StartServer();
+                break;
+            case "2":
+                Console.WriteLine("Joining Channel");
+                SocketClient.StartClient();
+                break;
+            case "3":
+                Console.WriteLine("I Just Want To Vibe Alone");
+                break;
+            default:
+                Console.WriteLine("Opps Wrong Choice");
+                break;
+        }
+    }
+}
